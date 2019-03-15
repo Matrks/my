3533879faf86f213dad306bc152669d6a4bdf826
Matrks <45499570+Matrks@users.noreply.github.com>
2019-03-15 21:30:00 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -23,22 +23,30 @@ client.on('message', message => {
   var argresult = message.content.split(` `).slice(1).join(' ');
     if (!devs.includes(message.author.id)) return;
     
-if (message.content.startsWith(adminprefix + 'setge')) {
+if (message.content.startsWith(adminprefix + 'ge')) {
   client.user.setGame(argresult);
     message.channel.sendMessage(`**${argresult} تم تغيير بلاينق  **`)
 } else 
-  if (message.content.startsWith(adminprefix + 'setna')) {
+  if (message.content.startsWith(adminprefix + 'na')) {
 client.user.setUsername(argresult).then
     message.channel.sendMessage(`**${argresult}** : تم تغيير أسم `)
 return message.reply("**لا يمكنك تغيير الاسم يجب عليك الانتظآر لمدة ساعتين . **");
 } else
-  if (message.content.startsWith(adminprefix + 'setav')) {
+  if (message.content.startsWith(adminprefix + 'av')) {
 client.user.setAvatar(argresult);
   message.channel.sendMessage(`**${argresult}** : تم تغير صورة `);
       } else     
-if (message.content.startsWith(adminprefix + 'setst')) {
+if (message.content.startsWith(adminprefix + 'st')) {
   client.user.setGame(argresult, "https://www.twitch.tv/idk");
-    message.channel.sendMessage(`**تم تغيير تويتش البوت إلى  ${argresult}**`)
+    message.channel.sendMessage(`**تم تغيير تويتش  إلى  ${argresult}**`)
+   if (message.content.startsWith(adminprefix + 'wt')) {
+  client.user.setActivity(argresult, {type:'WATCHING'});
+      message.channel.send(`**تَم تغيير الواتشينق الى   ${argresult}**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'l')) {
+  client.user.setActivity(argresult , {type:'LISTENING'});
+      message.channel.send(`**تَم تغيير الليسينينق الى   ${argresult}**`)
+  } else
 }
 });
 
